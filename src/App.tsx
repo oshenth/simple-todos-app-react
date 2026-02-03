65510a9b86d8e06ff6b832d82b30e43e65c2ade9
Oshenth Jayathilake <oshenth@gmail.com>
2026-02-03 09:41:26 +0530

    Code change to facilatate seperate Todolist component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
 import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -12,21 +13,23 @@ function App() {
     );
   }
 
+  function addTodo(title: string) {
+    setTodos((prevTodos) => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },
+      ...prevTodos,
+    ]);
+  }
+
   return (
     <main className="py-8 h-screen space-y-5`">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-50 rounded-md p-5 space-y-6">
-        <AddTodoForm />
-        <div className="space-y-2">
-          {todos.map((todo) => (
-            <TodoItem
-              key={todo.id}
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-            />
-          ))}
-          {}
-        </div>
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList todos={todos} onCompletedChange={setTodoCompleted} />
       </div>
     </main>
   );
